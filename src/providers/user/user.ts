d3f82f8a2f96b032b4aff9f228082bee4bc42cba
Colantuomo <pcolantuomo@gmail.com>
2017-09-03 15:02:29 -0300

    alterações no config

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http, RequestOptions, Headers } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Observable';
 
 /*
   Generated class for the UserProvider provider.
